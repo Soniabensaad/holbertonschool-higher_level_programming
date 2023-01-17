811a386e90d138d4571b9c52e8d65f2510808d07
Soniabensaad <5687@holbertonstudents.com>
2023-01-17 23:32:26 +0100

    upper

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for a in (str):
+        if ord(a) >= ord('a') and ord(a) <= ord('z'):
+            a = chr(ord(a) - 32)
+        print("{:s}".format(a), end="")
+    print("")
+       
