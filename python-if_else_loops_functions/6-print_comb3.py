af65e9f2cba6a12849b2ab9d78fe34a056ed2089
Soniabensaad <5687@holbertonstudents.com>
2023-01-17 20:44:07 +0100

    comb3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 for a in range(9):
-    for b in range(a+1,10):
-        if a!=b and a < 8:
-            print("{:d}{:d}".format(a,b), end=", ")
+    for b in range(a+1, 10):
+        if a!= b and a < 8:
+            print("{:d}{:d}".format(a, b), end=", ")
         else:
-            print("{:d}{:d}".format(a,b))
+            print("{:d}{:d}".format(a, b))
+
 
