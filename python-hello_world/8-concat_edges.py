8c8029619a1e1e996b2394342712848bc9ab989b
Soniabensaad <5687@holbertonstudents.com>
2023-01-17 15:13:44 +0100

    concat

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[38:66] +" "+ "with python"
+
+print(str)
