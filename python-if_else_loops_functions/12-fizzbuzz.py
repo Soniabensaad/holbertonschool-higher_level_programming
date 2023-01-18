7736d122395da620bd27da3a9a7a7eeab3653bd9
Soniabensaad <5687@holbertonstudents.com>
2023-01-18 13:44:42 +0100

    fizzzzzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1,101):
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
         elif i % 3 == 0:
@@ -8,4 +8,5 @@ def fizzbuzz():
         elif i % 5 == 0:
             print("Buzz", end=" ")
         else:
-            print(i, end=" ")
+            print("{}.format(i)", end=" ")
+
