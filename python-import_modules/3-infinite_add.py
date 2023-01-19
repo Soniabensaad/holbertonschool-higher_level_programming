a78b8c6a3354c87b608b6c8b7d7181cbaeba2c84
Soniabensaad <5687@holbertonstudents.com>
2023-01-19 11:03:27 +0100

    infinite add

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     arg = len(sys.argv)
     Sum = 0
-    for i in range (1,arg):
+    for i in range(1, arg):
         Sum += int(sys.argv[i])
     print(Sum)
