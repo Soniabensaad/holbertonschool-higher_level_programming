997874eb77cdb27f876d19abf483b1e2bf376f82
Soniabensaad <5687@holbertonstudents.com>
2023-01-19 10:47:56 +0100

    arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     import sys
     arg = sys.argv
     if len(arg) - 1 == 0:
-        print("{} arguments. ".format(len(arg) - 1, end=" "))
+        print("{} arguments.".format(len(arg) - 1, end=" "))
     elif len(arg) - 1 == 1:
-        print("{} argument: ".format(len(arg) - 1, end=" "))
+        print("{} argument:".format(len(arg) - 1, end=" "))
     else:
-        print("{} arguments: ".format(len(arg) - 1, end=" "))
+        print("{} arguments:".format(len(arg) - 1, end=" "))
 
     for i, argument in enumerate(arg):
         if(i == 0):
