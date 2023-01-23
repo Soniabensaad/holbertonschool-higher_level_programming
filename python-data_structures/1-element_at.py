573f1bb179f2b8c52c5aa99c86f7e5339941c3f3
Soniabensaad <5687@holbertonstudents.com>
2023-01-23 14:22:38 +0100

    print element

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     for idx in my_list:
         if idx < 0:
             return None
-        elif idx <= (len(my_list)):
+        elif idx <= (len(my_list)-1):
            return(my_list[idx])
         else:
             return None
