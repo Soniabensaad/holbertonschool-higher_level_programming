f50ba34c063670bd61512d391545429248e50495
Soniabensaad <5687@holbertonstudents.com>
2023-01-23 14:30:41 +0100

    print element

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for idx in my_list:
-        if (0 <= idx < len(my_list)):
-            return (my_list[idx])
-        else:
-            return (None)
-
+    if 0 <= idx < len(my_list):
+        return my_list[idx]
+    return None
