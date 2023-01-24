641334b29d9bcc4d3a071c7859bdae049e4e7ddb
Soniabensaad <5687@holbertonstudents.com>
2023-01-24 19:04:15 +0100

    maximal

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    max = 0
+    for i in range(len(my_list)):
+        if max < my_list[i]:
+            max = my_list[i]
+    return max
+
+
+
