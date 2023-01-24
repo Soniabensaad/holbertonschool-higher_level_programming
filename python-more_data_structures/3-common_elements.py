23ccb07e2f944d152eca35edd0266008ce5ee1dc
Soniabensaad <5687@holbertonstudents.com>
2023-01-24 22:50:00 +0100

    uniq sum

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -3,5 +3,5 @@ def common_elements(set_1, set_2):
     a_set = set(set_1)
     b_set = set(set_2)
     return list(a_set & b_set)
-    e = common(set_1,set_2)
+    e = common(set_1, set_2)
     print(e)
