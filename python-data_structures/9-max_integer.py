6ff36ac9273a9c608d1aa6c23ff7e8c82feb830b
Soniabensaad <5687@holbertonstudents.com>
2023-01-24 19:09:18 +0100

    maximal

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,6 +2,8 @@
 def max_integer(my_list=[]):
     if my_list == []:
         return None
+    if my_list[0] < 0:
+        return my_list[0]
     max = 0
     for i in range(len(my_list)):
         if max < my_list[i]:
