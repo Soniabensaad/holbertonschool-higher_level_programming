5a3b24b2ecd0ca071f95aae1c1a9c64d1173e13d
Soniabensaad <5687@holbertonstudents.com>
2023-01-25 22:07:01 +0100

    exception

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        c = 0
+        for i in range (x):
+            print("{}".format(i), end="")
+            c += 1
+        print()
+    except:
+        print()
+    return c
+    
+    
---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+    except:
+        return False
+
+
+    
+
+
+    
