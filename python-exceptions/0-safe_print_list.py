5ce054f499031d147e915eb977d1f884dc58c90a
Soniabensaad <5687@holbertonstudents.com>
2023-01-25 22:16:39 +0100

    exception

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,12 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     try:
         c = 0
-        for i in range (x):
+        for i in range(x):
             print("{}".format(my_list[i]), end="")
             c += 1
         print()
     except:
         print()
     return c
-    
-    
