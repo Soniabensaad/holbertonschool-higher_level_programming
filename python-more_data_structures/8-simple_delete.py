02262fc808dc153f0071525e95fd6a9fc0c850d1
Soniabensaad <5687@holbertonstudents.com>
2023-01-25 14:56:42 +0100

    delete key

---
+++ python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    for delete in a_dictionary:
+        if delete == key:
+            remove_key = a_dictionary.pop(key)
+            return a_dictionary
+    return a_dictionary
+        
