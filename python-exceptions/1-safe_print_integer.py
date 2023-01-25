36bde9ee7bfbddae8f16319de0760b50b86560f9
Soniabensaad <5687@holbertonstudents.com>
2023-01-25 22:23:17 +0100

    exception

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
+        isinstance(value, int)
+        print("{:d}".format(value))
+        return True
     except:
         return False
 
