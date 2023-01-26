e101692c87ee4f13b6d27c6e530bf6219a0dbd4f
Soniabensaad <5687@holbertonstudents.com>
2023-01-26 12:45:35 +0100

    listinteger

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        c = 0
-        for i in range(x):
+    c = 0
+    for i in range(x):
+        my_list[i]
+        try:
             if isinstance(my_list[i], int):
-                print("{}".format(my_list[i]), end="")
+                print("{:d}".format(my_list[i]), end="")
                 c += 1
-        print()
-    except:
-        print()
+        except ValueError and TypeError:
+            continue
+    print()
     return c
+    
+    
