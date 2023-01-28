0a29a73ff79438587ed6ddb0dd6cf2eff1a48e4b
Soniabensaad <5687@holbertonstudents.com>
2023-01-28 18:00:06 +0100

    integer error

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,7 @@
+import sys
+def safe_print_integer_err(value):
+    if isinstance(value, int):
+        print("{}".format(value))
+        return True
+    else:
+        sys.stderr.write("Exception: " + "False" + "\n")
