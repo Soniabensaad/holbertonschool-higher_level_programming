fa5bedea1e4f1d260994aa407fec3a64e438be9a
Soniabensaad <5687@holbertonstudents.com>
2023-01-28 18:16:28 +0100

    integer error

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -3,8 +3,8 @@ import sys
 def safe_print_integer_err(value):
     try:
         if isinstance(value, int):
-          print("{:d}".format(value))
-          return True
+            print("{:d}".format(value))
+            return True
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
