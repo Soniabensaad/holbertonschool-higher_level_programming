914fe14c26f062a424b5b48e3bcab177d7d8de66
Soniabensaad <5687@holbertonstudents.com>
2023-01-30 21:41:32 +0100

    square class

---
+++ python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+class that defines a square
+"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """get value of size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return square area"""
+        area = self.__size ** 2
+        return area
+    
+    def my_print(self):
+        """prints line"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+
