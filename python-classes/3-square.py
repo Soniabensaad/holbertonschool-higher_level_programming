1e38a021e8845f2ab7ac264cda80e66e0f39ac68
Soniabensaad <5687@holbertonstudents.com>
2023-01-30 20:50:57 +0100

    square class

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+class that defines a square
+"""
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0):
+        """initialize data"""
+        self.__size = size
+        if not isinstance (size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """return square area"""
+        area = self.__size ** 2
+        return area
+
+    
