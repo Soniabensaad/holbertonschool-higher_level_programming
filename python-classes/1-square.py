4ab73c6e29bae8afeb1e2d973e4eb6e5fcf6b0f3
Soniabensaad <5687@holbertonstudents.com>
2023-01-30 20:43:22 +0100

    square class

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class that defines a square"""
+class Square:
+    """class that defines a square"""
+
+    def __init__(self, size):
+        """initialize data"""
+        self.__size = size
+    
