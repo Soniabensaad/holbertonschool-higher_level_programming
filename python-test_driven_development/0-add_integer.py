c2d57d4acd962ea953e41dfc08ad442132aadb85
Soniabensaad <5687@holbertonstudents.com>
2023-01-31 16:39:03 +0100

    test drive

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+add_integer:
+    Checks if parameters are int
+    Returns sum of parameters
+"""
 def add_integer(a, b=98):
+    """
+    returns sum
+    """
     if type(a) == float or type(b) == float:
         a = int(a)
         b = int(b)
