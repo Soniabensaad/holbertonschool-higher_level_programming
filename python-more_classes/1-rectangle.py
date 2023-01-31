ec812d9beac071dc9ba03c2aba00ceead2545daa
Soniabensaad <5687@holbertonstudents.com>
2023-01-31 18:27:59 +0100

    class rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
     """
     def __init__(self, width=0, height=0):
         # attribute assigment here engages setters defined below
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
