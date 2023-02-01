87ac09f345c887b480a5957e6dd0872f2fc06750
Soniabensaad <5687@holbertonstudents.com>
2023-02-01 19:58:48 +0100

    matrix test driven

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -20,7 +20,8 @@ def matrix_divided(matrix, div):
         row = len(matrix[0])
         for i in matrix:
             if row != len(i):
-                raise TypeError("Each row of the matrix must have the same size")
+                raise TypeError("Each row of the matrix must\
+                    have the same size")
     else:
         raise("matrix must be a matrix (list of lists) of integers/floats")
     if not isinstance(div, (int, float)):
