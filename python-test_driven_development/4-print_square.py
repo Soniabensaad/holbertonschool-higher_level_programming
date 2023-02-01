ef30b40a06a7e65459b02ca34871b82a55feb5e9
Soniabensaad <5687@holbertonstudents.com>
2023-02-01 21:45:06 +0100

    print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+prints a square with the character #
+"""
+
+def print_square(size):
+    """prints a square with the character #"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        row = ""
+        for j in range(size):
+            row += "#"
+        print(row)
