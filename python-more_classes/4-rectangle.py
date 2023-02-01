2e54d8af05aed3e049f1a499a146451eb9df6654
Soniabensaad <5687@holbertonstudents.com>
2023-02-01 15:38:20 +0100

    class rectangle

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -87,7 +87,6 @@ class Rectangle:
             for i in range(self.__height):
                 rectangle += "#" * self.__width + "\n"
         return rectangle[:-1]
-    
     def __repr__(self):
         """Return the canonical string representation of the object"""
-        return "Rectangle({}, {})".format(self.__height, self.__width)
+        return 'Rectangle({}, {})'.format(self.__height, self.__width)
