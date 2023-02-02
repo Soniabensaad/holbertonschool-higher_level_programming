0434f440676eaf5bd27257932dc1e031117bcdb2
Soniabensaad <5687@holbertonstudents.com>
2023-02-02 20:07:31 +0100

    static methose

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
         if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """returns the biggest rectangle based on the area"""
