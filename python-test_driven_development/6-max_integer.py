e022e2132e0b4c4f95903c5dcc565236cf2f41e8
Soniabensaad <5687@holbertonstudents.com>
2023-02-02 17:05:02 +0100

    max integer

--- python-test_driven_development/6-max_integer.py
+++ python-test_driven_development/6-max_integer.py
@@ -10,7 +10,7 @@ def max_integer(list=[]):
     if len(list) == 0:
         return None
     max = list[0]
-    i = 1
+    i = 0
     while i < len(list):
         if list[i] > max:
             max = list[i]
