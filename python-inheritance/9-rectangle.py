88dba455cb7cba07dc60adb1441306f75e81a69b
Soniabensaad <5687@holbertonstudents.com>
2023-02-07 15:25:23 +0100

    rectangle print and return

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Geometry module
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """width and height must be positive integers,
+         validated by integer_validator"""
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+    
+    def area(self):
+        """returns area of rectangle"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width,self.__height )
