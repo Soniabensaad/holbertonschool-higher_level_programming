5631fc08d101929ba6344d62ec886c98df152e77
Soniabensaad <5687@holbertonstudents.com>
2023-02-07 20:25:48 +0100

    object oop leve

---
+++ python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Low memory cost"""
+class LockedClass:
+    """no class or object attribute
+    , that prevents the user from dynamically creating 
+    new instance attributes, 
+    except if the new instance attribute is called first_name."""
+    __slots__ = ["first_name"]
