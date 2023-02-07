d054f6e8a5bb055e51660ec99a563a9c50224334
Soniabensaad <5687@holbertonstudents.com>
2023-02-07 11:31:57 +0100

    base geometry new class

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Geometry module
+"""
+
+
+class BaseGeometry:
+    """ a class named BaseGeometry """
+    def area(self):
+        """raises an Exception
+        with the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
