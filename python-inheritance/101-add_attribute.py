51375a929db5d70a7c1cfbbdf05e23d66852d2bf
Soniabensaad <5687@holbertonstudents.com>
2023-02-07 18:01:42 +0100

    add attribute

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -6,7 +6,7 @@ Can I?
 
 def add_attribute(obj, attr, value):
     """ adds a new attribute to an object if it’s possible"""
-    no_add = (int, float, str, bool, tuple, range)
+    no_add = (int, float, str, bool, tuple, range, object, list, dict, type)
     if type(obj) in no_add:
         raise TypeError("can't add new attribute")
     obj.__setattr__(attr, value)
