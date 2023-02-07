e013adaa762ede1087fb3f139c32a616a2b448c0
Soniabensaad <5687@holbertonstudents.com>
2023-02-07 17:28:33 +0100

    class myint

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -4,11 +4,15 @@ My integer
 """
 
 class MyInt(int):
+    """class MyInt that inherits from int"""
     def __init__(self, value):
+        """initialisation"""
         self.value = value
 
     def __eq__(self, other):
+        """equal comparaison"""
         return self.value != other
 
     def __ne__(self, other):
+        """inequal comparaison"""
         return self.value == other
