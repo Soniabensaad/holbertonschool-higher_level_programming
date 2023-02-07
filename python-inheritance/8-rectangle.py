bcaa269fac7d9f20a74326aa03f2cd481c25ce61
Soniabensaad <5687@holbertonstudents.com>
2023-02-07 15:02:57 +0100

    base geometry test

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,6 +6,8 @@ Geometry module
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
+
+    
     """class Rectangle that inherits from BaseGeometry"""
     def __init__(self, width, height):
         """width and height must be positive integers,
