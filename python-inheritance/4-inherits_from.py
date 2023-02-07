c373f0a6edb752298ddb2fd9cce0c01bce1560b8
Soniabensaad <5687@holbertonstudents.com>
2023-02-07 11:04:42 +0100

    lookup

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,6 +5,9 @@ Only sub class of
 
 
 def inherits_from(obj, a_class):
+    """ function that returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class ; otherwise False"""
     if type(obj) is a_class or not isinstance(obj, a_class):
         return False
     else:
