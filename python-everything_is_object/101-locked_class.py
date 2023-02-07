984329e2f163e4ec03d1180d0d95c8718fb8b1a0
Soniabensaad <5687@holbertonstudents.com>
2023-02-07 20:28:02 +0100

    object oop leve

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -4,7 +4,7 @@
 
 class LockedClass:
     """no class or object attribute
-    , that prevents the user from dynamically creating 
-    new instance attributes, 
+    ,that prevents the user from dynamically creating 
+    new instance attributes,
     except if the new instance attribute is called first_name."""
     __slots__ = ["first_name"]
