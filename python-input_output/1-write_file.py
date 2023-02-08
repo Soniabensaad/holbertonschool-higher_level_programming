4611cb7a7ecda67244be5b412d6fc50a88a7ac85
Soniabensaad <5687@holbertonstudents.com>
2023-02-08 17:06:33 +0100

    append text in file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,5 +7,5 @@ Write to a file
 def write_file(filename="", text=""):
     """function that writes a string to a text file (UTF8)
     and returns the number of characters written:"""
-    with open(filename, "a", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return(f.write(text))
