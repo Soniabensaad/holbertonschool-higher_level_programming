f2a0e158efe64ce178be00647ef240c28877a66b
Soniabensaad <5687@holbertonstudents.com>
2023-02-08 17:36:04 +0100

    json to string

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-import json
 """
 To JSON string
 """
+import json
 
 
 def to_json_string(my_obj):
     """function that returns the JSON 
     representation of an object (string)"""
+
+    
     return json.dumps(my_obj)
