9e804cae424bd66bc58d57f8e09820ec780bcf93
Soniabensaad <5687@holbertonstudents.com>
2023-02-08 15:06:14 +0100

    len characters

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,7 +3,10 @@
 Write to a file
 """
 
+
 def write_file(filename="", text=""):
+    """function that writes a string to a text file (UTF8)
+    and returns the number of characters written:"""
     with open(filename, "a", encoding="utf-8") as f:
         return(f.write(text))
         
