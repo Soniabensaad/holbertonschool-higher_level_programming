b17cf5f692d83f6fb7797c77f6b14a0556720bcd
Soniabensaad <5687@holbertonstudents.com>
2023-02-10 09:07:15 +0100

    input json output

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -14,18 +14,19 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if attrs ==None or not isinstance(attrs, list):
-           return self.__dict__ 
+        if attrs == None or not isinstance(attrs, list):
+            return self.__dict__ 
         else:
             a = {}
             for i in attrs:
                 if not isinstance(i, str):
-                    return self.__dict__ 
+                    return self.__dict__
                 if i in self.__dict__.keys():
                     a[i] = self.__dict__[i]
             return a
-    
+
     def reload_from_json(self, json):
         for j in json.keys():
             self.__dict__[j] = json[j]
+
 
