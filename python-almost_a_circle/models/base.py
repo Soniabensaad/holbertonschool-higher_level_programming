3ae5953e59f33cc9ec62d19ee9c13ca1e279c153
Soniabensaad <5687@holbertonstudents.com>
2023-02-13 17:27:14 +0100

    circle oop project

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -9,13 +9,16 @@ class Base:
     Class Base:
      -private class attribute __nb_objects = 0
      -class constructor: def __init__(self, id=None)::
-          +if id is not None, assign the public instance attribute id with this argument value+
-          + increment __nb_objects and assign the new value to the public instance attribute id+
+          +if id is not None, assign the public instance attribute id
+           with this argument value+
+          + increment __nb_objects and assign 
+          the new value to the public instance attribute id+
     """
     """
     private class attribute __nb_objects without function
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
         """initialize id attribute"""
         if id is not None:
