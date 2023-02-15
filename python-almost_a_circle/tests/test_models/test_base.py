fd57e410c613b8616ff1dc6c62aa7578578e6ab7
Soniabensaad <5687@holbertonstudents.com>
2023-02-15 22:50:50 +0100

    test for almost a circle

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -5,21 +5,13 @@ Test cases for class Base.
 from models import Base
 import unittest
 class TestBase(unittest.TestCase):
-    """Test case for class Base."""
 
-
-    def test_1_0(self):
-        """Check for id."""
-
+    def test_id(self):
         b1 = Base()
         self.assertEqual(b1.id, 1)
         b2 = Base()
         self.assertEqual(b2.id, 2)
-        b3 = Base()
-        self.assertEqual(b3.id, 3)
-        b4 = Base(12)
-        self.assertEqual(b4.id, 12)
-        b5 = Base()
-        self.assertEqual(b5.id, 4)
-if __name__ == "__main__":
-    unittest.main()
+
+    def test_specified_id(self):
+        b3 = Base(89)
+        self.assertEqual(b3.id, 89)
