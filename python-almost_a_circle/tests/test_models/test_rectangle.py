2dec8ec7d9e440c9a9f494a4a558a93de0fdf86c
Soniabensaad <5687@holbertonstudents.com>
2023-02-16 16:13:56 +0100

    unitest program

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,40 +1,27 @@
 #!/usr/bin/python3
-"""Unittest rectangle.
-Test cases for class Rectangle.
-"""
-import unittest
-from models.base import Base
 from models.rectangle import Rectangle
+import unittest
 
 
-class TestRectangle(unittest.TestCase):
-    def test_rectangle_subclass(self):
-        """
-        Test if Rectangle class inherit from
-        Base class
-        """
-        self.assertTrue(issubclass(Rectangle, Base))
+class TestRectangle (unittest.TestCase):
 
-    def test_parameters(self):
-        """
-        Test parameters for Rectangle class
-        """
+    def test_simple_using(self):
         r1 = Rectangle(10, 2)
-        r2 = Rectangle(2, 10)
-        r3 = Rectangle(10, 2, 0, 0, 12)
-
-        self.assertEqual(r1.id, 4)
+        self.assertEqual(r1.id, 1)
         self.assertEqual(r1.width, 10)
-        self.assertEqual(r1.height, 2)
-        self.assertEqual(r1.x, 0)
-        self.assertEqual(r1.y, 0)
-        self.assertEqual(r2.id, 5)
-        self.assertEqual(r2.width, 2)
+
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.id, 2)
         self.assertEqual(r2.height, 10)
-        self.assertEqual(r2.x, 0)
-        self.assertEqual(r2.y, 0)
+
+        r2.x = 11
+        self.assertEqual(r2.x, 11)
+
+        r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
-        self.assertEqual(r3.width, 10)
-        self.assertEqual(r3.height, 2)
         self.assertEqual(r3.x, 0)
         self.assertEqual(r3.y, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
