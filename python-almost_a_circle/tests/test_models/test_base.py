bda7e1e137d0bdf892a3323f78655fb8f9baff8a
Soniabensaad <5687@holbertonstudents.com>
2023-02-16 13:24:43 +0100

    test for almost a circle

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,11 +2,13 @@
 """Unittest base.
 Test cases for class Base.
 """
-from models.base import Base
 import unittest
+from models.base import Base
+
+
 class TestBase(unittest.TestCase):
 
-    def test_id(self):
+    def test_new_instance_id(self):
         b1 = Base()
         self.assertEqual(b1.id, 1)
         b2 = Base()
@@ -16,16 +18,19 @@ class TestBase(unittest.TestCase):
         b3 = Base(89)
         self.assertEqual(b3.id, 89)
 
-    def test_json_to_string(self):
-        json_none = Base.to_json_string(None)
-        self.assertEqual(json_none, '[]')
+    def test_to_json_string_none(self):
+        rjson = Base.to_json_string(None)
+        self.assertEqual(rjson, '[]')
 
-        json_empty = Base.to_json_string([])
-        self.assertEqual(json_empty, '[]')
+        json_dictionary = Base.to_json_string([])
+        self.assertEqual(json_dictionary, '[]')
 
-        json_dictionnary_1 = Base.to_json_string([ { 'id': 12 }])
-        self.assertEqual(json_dictionnary_1, '[ { "id": 12 }]')
+        json_dictionary = Base.to_json_string([{'id': 12}])
+        self.assertEqual(json_dictionary, '[{"id": 12}]')
+
+    def test_from_json_string(self):
+        list_output = Base.from_json_string(None)
+        self.assertEqual(list_output, [])
 
 if __name__ == '__main__':
     unittest.main()
-
