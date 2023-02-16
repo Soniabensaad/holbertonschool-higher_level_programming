9abe228b5b1ecc1dc40158ebc4860f7a5d7c296c
Soniabensaad <5687@holbertonstudents.com>
2023-02-16 16:21:16 +0100

    unitest program

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,15 +1,1 @@
-#!/usr/bin/python3
-from models.rectangle import Rectangle
-import unittest
 
-
-class TestRectangle (unittest.TestCase):
-
-    def test_simple_using(self):
-        r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 1)
-        self.assertEqual(r1.width, 10)
-
-        r2 = Rectangle(2, 10)
-        self.assertEqual(r2.id, 2)
-        self.assertEqual(r2.height, 10)
