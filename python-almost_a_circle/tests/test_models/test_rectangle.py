2df71820707eb296656630356a87fd25683e4922
Soniabensaad <5687@holbertonstudents.com>
2023-02-16 16:19:36 +0100

    unitest program

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -13,15 +13,3 @@ class TestRectangle (unittest.TestCase):
         r2 = Rectangle(2, 10)
         self.assertEqual(r2.id, 2)
         self.assertEqual(r2.height, 10)
-
-        r2.x = 11
-        self.assertEqual(r2.x, 11)
-
-        r3 = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(r3.id, 12)
-        self.assertEqual(r3.x, 0)
-        self.assertEqual(r3.y, 0)
-
-
-if __name__ == "__main__":
-    unittest.main()
