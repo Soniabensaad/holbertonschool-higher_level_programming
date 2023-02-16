7e8b99bf41523611d15f1669b26a95c58ae8f1e2
Soniabensaad <5687@holbertonstudents.com>
2023-02-16 17:18:33 +0100

    test for almost a circle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,32 +1,27 @@
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
-from models.square import Square
 
 
 class TestRectangle(unittest.TestCase):
     """Test cases for the Rectangle class."""
 
     
+    def setUp(self):
+        Base._Base__nb_objects = 0
+    def test_id(self):
 
-    def test_2_0(self):
-
-        r0 = Rectangle(1, 2)
-        self.assertEqual(r0.id, 1)
-        r1 = Rectangle(2, 3)
-        self.assertEqual(r1.id, 2)
-        r2 = Rectangle(3, 4)
+        r1 = Rectangle(1, 2)
+        self.assertEqual(r1.id, 1)
+        r2 = Rectangle(3, 4, 5)
         self.assertEqual(r2.id, 3)
-        r3 = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(r3.id, 12)
-        r4 = Rectangle(10, 2, 4, 5, 34)
-        self.assertEqual(r4.id, 34)
-        r5 = Rectangle(10, 2, 4, 5, -5)
-        self.assertEqual(r5.id, -5)
-        r6 = Rectangle(10, 2, 4, 5, 9)
-        self.assertEqual(r6.id, 9)
+        r3 = Rectangle(10, 2, 0, 0)
+        self.assertEqual(r3.id, -5)
+        r4 = Rectangle(10, 2, 4, 5, 30)
+        self.assertEqual(r4.id, 30)
+        
 
-    def test_2_1(self):
+    def test_width_height_x_y(self):
 
         r1 = Rectangle(10, 2)
         self.assertEqual(r1.width, 10)
