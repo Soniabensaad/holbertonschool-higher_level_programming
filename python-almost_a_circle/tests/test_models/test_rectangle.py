f40ba588a2e5e3c28cc8c422b24db0ba783893b9
Soniabensaad <5687@holbertonstudents.com>
2023-02-16 14:12:20 +0100

    test for almost a circle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,14 +7,15 @@ from models.rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
-    def test_Rectangle(self):
+     def test_Rectangle(self):
         r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 3)
+        self.assertEqual(r1.id, 1)
 
-        r2 = Rectangle(1, 2, 3)
-        self.assertEqual(r2.id, 4)
+        r2 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(r2.id, 2)
 
-        r3 = Rectangle(1, 2, 3, 4)
-        self.assertEqual(r3.id, 5)
+        r3 = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(r3.id, 6)
+
 if __name__ == '__main__':
     unittest.main()
