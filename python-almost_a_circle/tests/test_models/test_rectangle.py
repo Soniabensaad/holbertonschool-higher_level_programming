ded5ef1d4e47b52053bdd87c9e662be87ae79477
Soniabensaad <5687@holbertonstudents.com>
2023-02-16 16:51:37 +0100

    prohject so hard to do

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,10 +3,32 @@
 Test cases for class Base.
 """
 import unittest
+from models.base import Base
 from models.rectangle import Rectangle
 
-class TestRectangle(unittest.TestCase):
-    def test_rectangle(self):
-        r1 = Rectangle(1, 2)
-        self.assertEqual(r1.id , 1)
-        self.assertEqual(r1.height, 2)
+
+class TestRectangleMethods(unittest.TestCase):
+    """ Defines tests for Rectangle class """
+
+  
+    
+
+    def test_randos_id(self):
+        """ Test random arguments passed """
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(2, 10)
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        r4 = Rectangle(2, 10)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r2.id, 2)
+        self.assertEqual(r3.id, 12)
+        self.assertEqual(r4.id, 3)
+
+    def test_class(self):
+        """ Test Rectangle class type """
+        self.assertEqual(str(Rectangle),
+                         "<class 'models.rectangle.Rectangle'>")
+
+    def test_class_inheritance(self):
+        """ Test if Rectangle inherits from Base """
+        self.assertTrue(issubclass(Rectangle, Base))
