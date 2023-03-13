4eaef13684efcdff2d5abaabf0e9220ac78e22e5
Soniabensaad <5687@holbertonstudents.com>
2023-03-13 20:03:08 +0100

    mysql project

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- creates a table second_table in the database hbtn_0c_0 in your MySQL
+--  inserts a new row in the table first_table (database hbtn_0c_0)
+INSERT INTO first_table (id, name)
+VALUES (89, 'Best School');
+
+
+INSERT INTO second_table (id, name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
