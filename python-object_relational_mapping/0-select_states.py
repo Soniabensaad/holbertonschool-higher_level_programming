88bd95a0ce22805d55d8e4aee4d184fa0ead4d9a
Soniabensaad <5687@holbertonstudents.com>
2023-03-15 21:59:04 +0100

    project hard database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,12 +2,15 @@
 """script that lists all states from the database hbtn_0e_0_usa"""
 import sys
 import MySQLdb
-if __name__== "__main__":
+if __name__ == "__main__":
     mysql_user = sys.argv[1]
     mysql_pwd = sys.argv[2]
     db_name = sys.argv[3]
-    db = MySQLdb.connect(host='localhost', port=3306,
-                          user=mysql_user, passwd=mysql_pwd, db=db_name)
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     for state in cur.fetchall():
