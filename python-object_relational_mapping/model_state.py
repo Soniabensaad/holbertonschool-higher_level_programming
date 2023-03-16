1a3dfa82e8e8d54742ff2f88841fdb742c1a0747
Soniabensaad <5687@holbertonstudents.com>
2023-03-16 20:07:27 +0100

    project hard

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Write a python file that contains 
+"""Write a python file that contains
 the class definition of a State """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
 class State(Base):
     """from sqlalchemy import Column, Integer, String
     from sqlalchemy.ext.declarative import declarative_base
     Base = declarative_base()
     """
-    __tablename__='states'
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name =  Column(String(128), nullable=False)
