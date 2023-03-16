776b6e5484fc4b713c234f44b9dd18268f13ebee
Soniabensaad <5687@holbertonstudents.com>
2023-03-16 23:06:43 +0100

    project hard

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
                            (argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    dlt = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    dlt = session.query(State).filter(
+        State.name.like('%a%')).order_by(State.id).all()
     for obj in dlt:
         session.delete(obj)
     session.commit()
