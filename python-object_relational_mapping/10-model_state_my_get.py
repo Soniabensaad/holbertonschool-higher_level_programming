1596f5da0b4e2c858e30673be1d06655883c2003
Soniabensaad <5687@holbertonstudents.com>
2023-03-16 21:50:17 +0100

    project hard

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""prints the State object with the name passed as argument from the database"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    state = session.query(State).filter(
+        State.name == argv[4]).first()
+    if state is None:
+        print("Not found")
+    else:
+        print(state.id)
+    session.close()
