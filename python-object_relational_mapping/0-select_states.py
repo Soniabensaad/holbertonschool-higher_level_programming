20189948273d4dc307c6ba880880d39ab2293558
Soniabensaad <5687@holbertonstudents.com>
2023-03-16 12:36:39 +0100

    project hard

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 """script that lists all states from the database hbtn_0e_0_usa"""
 import sys
 import MySQLdb
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
+"""lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
 import sys
 import MySQLdb
 if __name__ == "__main__":
