8eb9c4309dcb934e5e98c83505a5ed9345048a1a
Soniabensaad <5687@holbertonstudents.com>
2023-03-16 13:41:21 +0100

    project hard

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all values 
+in the states table of hbtn_0e_0_usa
+where name matches the argument."""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        user = sys.argv[1]
+    else:
+        print("Error: no user provided.")
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    db_s = sys.argv[4]
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name = '{}'\
+              ORDER BY states.id ASC".format(db_s))
+    for state in c.fetchall():
+        print(state)
+    c.close()
+    db.close()
