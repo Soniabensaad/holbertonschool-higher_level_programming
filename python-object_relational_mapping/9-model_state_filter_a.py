29efe0211734f3b0fc00c1f1bdfe4968a63c89ed
Soniabensaad <5687@holbertonstudents.com>
2023-03-16 21:19:15 +0100

    project hard

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
                            (argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    state = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+    state = session.query(State).filter(
+        State.name.like('%a%')).order_by(State.id)
     for state in state.all():
         print("{}: {}".format(state.id, state.name))
     session.close()
