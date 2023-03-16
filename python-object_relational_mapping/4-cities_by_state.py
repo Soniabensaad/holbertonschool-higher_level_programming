66cfccf3c1a46d02b53265061a01c9c524539930
Soniabensaad <5687@holbertonstudents.com>
2023-03-16 17:27:40 +0100

    project hard

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     c.execute("SELECT cities.id, cities.name, states.name FROM cities\
                 INNER JOIN states ON cities.state_id = states.id\
                 ORDER BY cities.id ASC")
-    for state in c.fetchall():
-        print(state)
+    for row in c.fetchall():
+        print(row)
     c.close()
     db.close()
