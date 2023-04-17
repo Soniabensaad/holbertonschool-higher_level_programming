233f463db50f748c54686d06e720f1753895e6a8
Soniabensaad <5687@holbertonstudents.com>
2023-04-17 14:52:18 +0200

    javascript work

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 // Prints a message depending of the number of arguments passed
 
 if (process.argv[2]) {
-    console.log(process.argv[2]);
-  } else {
-    console.log('No argument');
-  }
+  console.log(process.argv[2]);
+} else {
+  console.log('No argument');
+}
