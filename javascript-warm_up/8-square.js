2e4a1729ee6bc4edd9142aa5ac1896082d66fbf4
Soniabensaad <5687@holbertonstudents.com>
2023-04-17 17:38:02 +0200

    javascript work

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 const size = parseInt(process.argv[2]);
 
 if (isNaN(size)) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
     console.log('X'.repeat(size));
