b2c19c971a0b07c90d8ff8e0c112910ab140987f
Soniabensaad <5687@holbertonstudents.com>
2023-04-17 14:35:54 +0200

    javascript work

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 // Prints a message depending of the number of arguments passed
 
 if (process.argv.length === 2) {
-    console.log('No argument');
-  } else if (process.argv.length === 3) {
-    console.log('Argument found');
-  } else {
-    console.log('Arguments found');
-  }
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
