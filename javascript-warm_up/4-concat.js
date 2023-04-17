00fc7e824c7e6405717b9fe155313b098be60670
Soniabensaad <5687@holbertonstudents.com>
2023-04-17 15:06:46 +0200

    javascript work

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-//two arguments passed to it, in the following format: “ is ”
+// two arguments passed to it, in the following format: “ is ”
 if (process.argv[2] && process.argv[3]) {
-    console.log(process.argv[2] + " is " + process.argv[3]);
-  } else if (process.argv[2]) {
-    console.log(process.argv[2] + " is " +"undefined");
-  } else {
-    console.log("undefined" + " is " +"undefined");
-  }
-  
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+} else if (process.argv[2]) {
+  console.log(process.argv[2] + ' is ' + 'undefined');
+} else {
+  console.log('undefined' + ' is ' + 'undefined');
+}
