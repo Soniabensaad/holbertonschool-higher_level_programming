8224ae8e60f9f5ddbdde97bdd3cdecbfdfd660a3
Soniabensaad <5687@holbertonstudents.com>
2023-04-17 17:55:46 +0200

    javascript work

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
-//prints the addition of 2 integers
+// prints the addition of 2 integers
 const add = (a, b) => {
-    if (!isNaN(a) && !isNaN(b)) {
-      console.log(a + b);
-    } else {
-      console.log("NaN");
-    }
+  if (!isNaN(a) && !isNaN(b)) {
+    console.log(a + b);
+  } else {
+    console.log('NaN');
   }
-  
-  const a = parseInt(process.argv[2]);
-  const b = parseInt(process.argv[3]);
-  
-  add(a, b);
-  
+};
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+add(a, b);
