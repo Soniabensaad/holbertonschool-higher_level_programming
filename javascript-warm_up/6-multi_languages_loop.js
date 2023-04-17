841bcf705e2da41ae9e30bcd401393d68ead2efe
Soniabensaad <5687@holbertonstudents.com>
2023-04-17 16:29:54 +0200

    javascript work

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//prints 3 lines by using an array of string and a loop
+const list = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (let i = 0; i < list.length; i++)
+{
+    console.log(list[i]);
+}
