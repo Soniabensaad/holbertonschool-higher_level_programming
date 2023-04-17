ebf322f64ad64e6a914b7b8a194de68c5a298302
Soniabensaad <5687@holbertonstudents.com>
2023-04-17 16:46:36 +0200

    javascript work

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
-//script that prints x times “C is fun”
+// script that prints x times “C is fun”
 const n = parseInt(process.argv[2]);
-if (!isNaN (n))
-{
-    for (let i = 0; i < n; i++) {
-        console.log("C is fun");
-    }
-    
-
-}
-else
-{
-    console.log("Missing number of occurrences");
-
+if (!isNaN(n)) {
+  for (let i = 0; i < n; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
