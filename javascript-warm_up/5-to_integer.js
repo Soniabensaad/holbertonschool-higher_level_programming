a7634e6befc232b31291759f43510f08256e6c70
Soniabensaad <5687@holbertonstudents.com>
2023-04-17 16:19:21 +0200

    javascript work

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 //prints My number: <first argument converted in integer>
-if (process.argv[2]) {
+if (!isNaN(process.argv[2])) {
     console.log("My number:", parseInt(process.argv[2]));
   } else {
     console.log('Not a number');
