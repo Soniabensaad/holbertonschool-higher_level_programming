1b94ab679571b2c2c1124ec075d57a6e559cb3da
Soniabensaad <5687@holbertonstudents.com>
2023-04-17 21:21:28 +0200

    javascript work

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || !Number.isInteger(w) || h <= 0 || !Number.isInteger(h)) {
-      
-        return this;
-      }
-      this.width = w;
-      this.height = h;
+  constructor (w, h) {
+    if (w <= 0 || !Number.isInteger(w) || h <= 0 || !Number.isInteger(h)) {
+      return this;
     }
+    this.width = w;
+    this.height = h;
   }
-  
-  module.exports = Rectangle;
+}
+
+module.exports = Rectangle;
