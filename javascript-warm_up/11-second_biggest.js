493995e633d82d08fa4c4fa788dc41e2be4e7122
Soniabensaad <5687@holbertonstudents.com>
2023-04-18 17:37:57 +0200

    factoriel

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-//searches the second biggest integer in the list of argument
-if(process.argv.length<= 3){
-    console.log(0);
-}
-else{
-    const rst = (process.argv.sort());
-    console.log(rst.reverse()[1]);
+// searches the second biggest integer in the list of argument
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const rst = process.argv.sort();
+  console.log(rst.reverse()[1]);
 }
