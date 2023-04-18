92cdaf3f51c9b52e5e783b8e7f2328be9f6a18ff
Soniabensaad <5687@holbertonstudents.com>
2023-04-18 19:39:31 +0200

    factoriel

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,25 +1,24 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w <= 0 || !Number.isInteger(w) || h <= 0 || !Number.isInteger(h)) {
-        return this;
+    constructor(w, h) {
+      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+        return {};
       }
       this.width = w;
       this.height = h;
     }
+  
     print() {
-        if (!this.width || !this.height) {
-            return;
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += 'X';
         }
-        for (let i = 0; i < this.height ; i++){
-            let row ="";
-            for (let j = 0; i< this.width; j++){
-                 row += "X";
-
-            }
-            console.log (row);
-        }
+        console.log(row);
+      }
     }
   }
-
+  
+  module.exports = Rectangle;
+  
   
