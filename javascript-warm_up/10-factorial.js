eb4f654238de1ae554a01a2594866acbf9b63d76
Soniabensaad <5687@holbertonstudents.com>
2023-04-18 17:02:26 +0200

    factoriel

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
-//script that computes and prints a factorial
+// script that computes and prints a factorial
 
-function factorial(n) {
-        if (isNaN(n)) {
-          return 1;
-        }
-        if (n <= 1) {
-          return 1;
-        }
-        return n * factorial(n - 1);
-      }
-      
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
 const input = parseInt(process.argv[2]);
 console.log(factorial(input));
-      
