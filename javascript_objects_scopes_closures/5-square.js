9e480ddd592f1f33ef69db1832f883679c3f08ce
Soniabensaad <5687@holbertonstudents.com>
2023-04-18 21:34:08 +0200

    factoriel

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
+const Rectangle = require('./4-rectangle');
 
-const Rectangle = require('./Rectangle');
-class Square extends Rectangle{
-    constructor(size) {
-        super(size, size);
-    }
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
 }
+
 module.exports = Square;
