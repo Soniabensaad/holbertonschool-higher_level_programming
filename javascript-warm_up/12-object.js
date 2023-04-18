897cbb8e8054dc01d3d7f2aa8350504624ba3bf5
Soniabensaad <5687@holbertonstudents.com>
2023-04-18 17:56:50 +0200

    factoriel

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 // replace the value 12 with 89
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  
-  console.log(myObject);
-  
-  const newObj = {
-    ...myObject, 
-    value: 89 
-  };
-  
-  console.log(newObj);
+  type: 'object',
+  value: 12
+};
+
+console.log(myObject);
+
+const newObj = {
+  ...myObject,
+  value: 89
+};
+
+console.log(newObj);
