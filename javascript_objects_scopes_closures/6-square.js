0209790debecbca0f39d79c09f74f1aec3044b45
Soniabensaad <5687@holbertonstudents.com>
2023-04-19 16:46:04 +0200

    factoriel

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-
 const Rectangle = require('./5-rectangle');
+
 class Square extends Rectangle {
     constructor (size) {
       super(size, size);
@@ -19,5 +19,4 @@ class Square extends Rectangle {
       }
     }
   }
-
-module.exports = Square;
+  
