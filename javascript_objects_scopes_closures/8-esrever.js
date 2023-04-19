552b65b490d676d0a0b4f88f530c7939ac76786d
Soniabensaad <5687@holbertonstudents.com>
2023-04-19 17:48:22 +0200

    factoriel

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-exports.esrever = function (list){
-    let c = [];
-    for(let i= list.length -1; i>=0; i--)
-    {
-        
-        c.push(list[i]);
-    }
-    return(c);
-}
+exports.esrever = function (list) {
+  const c = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    c.push(list[i]);
+  }
+  return (c);
+};
