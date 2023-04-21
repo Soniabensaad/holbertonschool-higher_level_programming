8809cc882ccd0f0dea4e953c4447555de2c5e2b2
Soniabensaad <5687@holbertonstudents.com>
2023-04-22 00:09:25 +0200

    js javascript

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,8 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const rst = process.argv.sort();
-  console.log(rst.reverse()[1]);
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
