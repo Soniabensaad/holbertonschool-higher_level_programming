34d58dbee77324e3d0a52f1eb57db1f737ad5a7a
Soniabensaad <5687@holbertonstudents.com>
2023-04-24 20:06:48 +0200

    read javascript

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// prints the title of a Star Wars movie where 
+//the episode number matches a given integer.
+const request = require('request');
+episode  = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${episode}`;
+request.get(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    else{
+        const titleMovie = JSON.parse(body);
+        console.log(titleMovie.title);
+    }
+  });
