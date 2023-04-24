c32d5227d084e84ade309cd208e1ab65fe3468e6
Soniabensaad <5687@holbertonstudents.com>
2023-04-24 14:52:38 +0200

    read javascript

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//writes a string to a file.
+// writes a string to a file.
 const fs = require('fs');
 
 const filePath = process.argv[2];
@@ -9,4 +9,4 @@ fs.writeFile(filePath, textToWrite, 'utf-8', function (err) {
   if (err) {
     console.log(err);
   }
-})
+});
