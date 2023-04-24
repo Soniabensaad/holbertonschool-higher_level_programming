0a820831f0945cb331cda1808ad36fd9cc54f9b2
Soniabensaad <5687@holbertonstudents.com>
2023-04-24 21:45:40 +0200

    read javascript

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,18 @@
 #!/usr/bin/node
 // number of movies where the character “Wedge Antilles” is present
 const request = require('request');
-const id = 18;
-const url = `https://swapi-api.hbtn.io/api/people/${id}/`;
 
-request(url, (error, response, body) => {
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+request(apiUrl, function(error, response, body) {
   if (error) {
     console.error(error);
-    return;
+  } else {
+    const films = JSON.parse(body).results;
+    const numFilmsWithWedgeAntilles = films.filter(film => {
+      return film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`);
+    }).length;
+    console.log(numFilmsWithWedgeAntilles);
   }
-
-  const filmsUrl = JSON.parse(body).films;
-  const filmsCount = filmsUrl.length;
-  console.log(filmsCount);
 });
