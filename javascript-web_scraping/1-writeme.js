a7d38857dba7e378f9d9245807cb388c29a08833
Soniabensaad <5687@holbertonstudents.com>
2023-04-24 14:50:52 +0200

    read javascript

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 //writes a string to a file.
-const path = process.argv[2];
-const text = process.argv[3];
 const fs = require('fs');
-fs.writeFile(path, text, 'utf-8', (err, data)=>{
-    if(err){
-        throw err;
-    }
-    console.log(data);
+
+const filePath = process.argv[2];
+const textToWrite = process.argv[3];
+
+fs.writeFile(filePath, textToWrite, 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
+  }
 })
