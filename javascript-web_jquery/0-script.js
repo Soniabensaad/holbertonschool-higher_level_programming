8dde268d52fbae2a1ecbb278727fe16c28d12e08
Soniabensaad <5687@holbertonstudents.com>
2023-04-25 15:38:04 +0200

    js query

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+//updates the text color of the <header> 
+//element to red (#FF0000)
+const headerElement = document.querySelector('header');
+headerElement.style.color = '#FF0000';
---
+++ javascript-web_jquery/README.md
@@ -0,0 +1,3 @@
+
+Project badge
+JavaScript - Web jQuery
