9dd7bf94bd4db8f30b9ef40f54aed0abfd2cb8b4
Soniabensaad <5687@holbertonstudents.com>
2023-04-25 14:03:25 +0200

    js request

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,28 @@
 #!/usr/bin/node
 //computes the number of tasks completed by user id.
+
 const request = require('request');
 
-const url = "https://jsonplaceholder.typicode.com/todos";
-request(url, (error, response, body)=>{
-    if(error){
-        console.log(error);
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedByUser = {};
+
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      if (completedByUser[todo.userId]) {
+        completedByUser[todo.userId] += 1;
+      } else {
+        completedByUser[todo.userId] = 1;
+      }
     }
-    else {
-        const todos = JSON.parse(body);
-        const completed = {};
-        todos.forEach(todo => {
-            if(todo.completed){
-                const userId = todo.userId;
-                completed[userId] = (completed[userId] ||0)+1;
-            }
-        });
-        for (const [userId, count] of Object.entries(completed)) {
-            console.log(`${userId}: ${count}`);
-        }
-    }
+  });
+
+  console.log(completedByUser);
 });
