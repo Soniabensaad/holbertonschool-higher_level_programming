b5d322fef2a1665ce9ddb46c789ebc91c5c642e4
Soniabensaad <5687@holbertonstudents.com>
2023-04-25 12:38:17 +0200

    js request

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+//gets the contents of a webpage and stores it in a file.
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } 
+   else {
+    fs.writeFile(filePath, body, { encoding: 'utf8' }, function (err) {
+      if (err) {
+        console.error(err);
+      } 
+    });
+  }
+});
