ff952d5bac38904d06aa8865356225fe72b06c2c
Soniabensaad <5687@holbertonstudents.com>
2023-05-31 22:52:37 +0200

    python network

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-# script to send custom headers to servers
-curl -sX GET $1 -H "X-HolbertonSchool-User-Id: 98" -L
+
+# Read the URL from command-line argument
+url="$1"
+
+# Set the header variable
+header="X-HolbertonSchool-User-Id: 98"
+
+# Send a GET request using curl, include the custom header, and store the response in a variable
+response=$(curl -s -H "$header" "$url")
+
+# Display the body of the response
+echo "$response"
