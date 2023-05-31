81a1ff0dfc5f6a214994d6f0bff9f9aee19eb2c8
Soniabensaad <5687@holbertonstudents.com>
2023-05-31 16:54:32 +0200

    python network

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # Read the URL from command-line argument
-url="$1"
-# Send a request using curl and store the response in a variable
-response=$(curl -sI "$url")
-# Extract the Content-Length header from the response headers
-content_length=$(echo "$response" | grep -i 'Content-Length' | awk '{print $2}' | tr -d '\r')
-# Display the size of the body in bytes
-echo "Size of the response body: $content_length bytes"
+curl -s -v "$1" --stderr - | grep Content-Length: | cut -d' '  -f 3
+
