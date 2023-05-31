98c25385e164468b5caf605c065b7966ce5891a8
Soniabensaad <5687@holbertonstudents.com>
2023-05-31 22:35:31 +0200

    python network

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a GET request to the URL,
+# Bash script that takes in a URL, sends a GET request to the URL
 if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
