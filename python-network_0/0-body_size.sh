a6985f89a2a73d6869d6e3a25149d7d30df4a53b
Soniabensaad <5687@holbertonstudents.com>
2023-05-31 16:58:45 +0200

    python network

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Read the URL from command-line argument
-curl -sI $1 | grep "Content-Length" | cut -d " " -f2
+# script to get the body size of a request
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
