1164a0e742ed01c33685a4c4599b812212ab0bc9
sonia <https://github.com/Soniabensaad>
2023-06-06 14:37:51 +0200

    hello

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import urllib.request
 import sys
-
+#  Python script that takes in a URL, sends a request to the URL and displays 
+# the value of the X-Request-Id variable found in the header
 url = sys.argv[1]
 
 # Send a GET request to the URL
