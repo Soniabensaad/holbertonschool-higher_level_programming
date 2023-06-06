185015e5c82a18b42093fdef2f91dde5b3b47888
sonia <https://github.com/Soniabensaad>
2023-06-06 14:57:35 +0200

    hello

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -7,12 +7,12 @@ import urllib.parse
 import sys
 
 if __name__ == "__main__":
-   url = sys.argv[1]
-   email = sys.argv[2]
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-   data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
-   with urllib.request.urlopen(url, data=data) as response:
+    with urllib.request.urlopen(url, data=data) as response:
     
-      content = response.read().decode('utf-8')
-      print(content)
+       content = response.read().decode('utf-8')
+       print(content)
