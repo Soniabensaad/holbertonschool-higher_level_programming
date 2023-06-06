e26414473c8be4f500307df078a1dfaa9a782868
sonia <https://github.com/Soniabensaad>
2023-06-06 14:42:36 +0200

    hello

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays 
-the value of the X-Request-Id variable found in the header
-"""
+""" sends a request to the URL and displays the value of
+ the X-Request-Id variable found in the header."""
 import urllib.request
-import sys
+from sys import argv
 
-url = sys.argv[1]
-
-with urllib.request.urlopen(url) as response:
-    
-    x_request_id = response.getheader('X-Request-Id')
-
-    print(x_request_id)
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as response:
+        print(response.getheader('X-Request-Id'))
